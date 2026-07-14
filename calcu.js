f96d5f0195ed1a0ff07e9e70de34295691920398
Zaid Shuqair <zaidshuqairbizz@gmail.com>
2026-07-14 21:36:11 +0300

    added logic for the four operations created number obj to hold user inputs

---
+++ calcu.js
@@ -0,0 +1,21 @@
+let number = {
+    leftSide: null,
+    rightSide: null
+}
+
+function add(leftSide, rightSide){
+    +leftSide + +rightSide
+}
+
+function substract(leftSide, rightSide){
+    +leftSide - +rightSide
+}
+
+function divide(leftSide, rightSide){
+    +leftSide / +rightSide
+}
+
+function multiply(leftSide, rightSide){
+    +leftSide * +rightSide
+}
+
